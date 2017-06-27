bf9a3c0bb37c952b9357f43fbf9b0ad87ee762dc
Khaya Mathebula <khayaprom@gmail.com>
2017-06-27 22:12:03 +0200

    created class members and implemented their listeners

--- app/src/main/java/za/co/aeonsource/quizapp/QuizActivity.java
+++ app/src/main/java/za/co/aeonsource/quizapp/QuizActivity.java
@@ -2,12 +2,41 @@ package za.co.aeonsource.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button_id);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this, "True Clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mFalseButton = ((Button) findViewById(R.id.false_button));
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this, "False Clicked!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate menu on this method
+        return true;
     }
 }
